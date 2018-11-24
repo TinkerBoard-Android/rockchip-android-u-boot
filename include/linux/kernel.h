429faa29e1a080f54ea40424125f1404fdc5a6f4
Simon Glass <sjg@chromium.org>
2018-11-23 21:29:42 -0700

    Add UINT32_MAX and UINT64_MAX
    These constants are defined by stdint.h but not by kernel.h, which is
    its stand-in in U-Boot. Add the definitions so that libraries which expect
    stdint.h constants can work.
    
    Change-Id: I52f2aa3fde452083f97e0053328441ed4da25a4f
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -33,7 +33,11 @@
 #define S64_MAX		((s64)(U64_MAX>>1))
 #define S64_MIN		((s64)(-S64_MAX - 1))
 
-#define INT32_MAX      S32_MAX
+/* Aliases defined by stdint.h */
+#define UINT32_MAX	U32_MAX
+#define UINT64_MAX	U64_MAX
+
+#define INT32_MAX	S32_MAX
 
 #define STACK_MAGIC	0xdeadbeef
 
