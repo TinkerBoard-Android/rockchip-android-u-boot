858ab46d9f33c110eb185dff34307a03f5d66814
Simon Glass <sjg@chromium.org>
2020-03-18 11:44:03 -0600

    fit_check_sign: Allow selecting the configuration to verify
    This tool always verifies the default configuration. It is useful to be
    able to verify a specific one. Add a command-line flag for this and plumb
    the logic through.
    
    Change-Id: I267e1377e74f89152dd0317b0aaf7a555d0239f3
    Signed-off-by: Simon Glass <sjg@chromium.org>

--- tools/fit_check_sign.c
+++ tools/fit_check_sign.c
@@ -42,6 +42,7 @@ int main(int argc, char **argv)
 	void *fit_blob;
 	char *fdtfile = NULL;
 	char *keyfile = NULL;
+	char *config_name = NULL;
 	char cmdname[256];
 	int ret;
 	void *key_blob;
@@ -50,7 +51,7 @@ int main(int argc, char **argv)
 
 	strncpy(cmdname, *argv, sizeof(cmdname) - 1);
 	cmdname[sizeof(cmdname) - 1] = '\0';
-	while ((c = getopt(argc, argv, "f:k:s")) != -1)
+	while ((c = getopt(argc, argv, "f:k:s:c")) != -1)
 		switch (c) {
 		case 'f':
 			fdtfile = optarg;
@@ -60,6 +61,9 @@ int main(int argc, char **argv)
 			break;
 		case 's':
 			is_spl = 1;
+			break;
+		case 'c':
+			config_name = optarg;
 			break;
 		default:
 			usage(cmdname);
@@ -83,7 +87,7 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 
 	image_set_host_blob(key_blob);
-	ret = fit_check_sign(fit_blob, key_blob, is_spl);
+	ret = fit_check_sign(fit_blob, key_blob, is_spl, config_name);
 	if (!ret) {
 		ret = EXIT_SUCCESS;
 		fprintf(stderr, "Signature check OK\n");
--- tools/image-host.c
+++ tools/image-host.c
@@ -736,12 +736,13 @@ int fit_add_verification_data(const char *keydir, void *keydest, void *fit,
 }
 
 #ifdef CONFIG_FIT_SIGNATURE
-int fit_check_sign(const void *fit, const void *key, int is_spl)
+int fit_check_sign(const void *fit, const void *key, int is_spl,
+		   const char *fit_uname_config)
 {
 	int cfg_noffset;
 	int ret;
 
-	cfg_noffset = fit_conf_get_node(fit, NULL);
+	cfg_noffset = fit_conf_get_node(fit, fit_uname_config);
 	if (!cfg_noffset)
 		return -1;
 
@@ -750,6 +751,7 @@ int fit_check_sign(const void *fit, const void *key, int is_spl)
 	ret = fit_config_verify(fit, cfg_noffset);
 	if (ret)
 		return ret;
+	printf("Verified OK, loading images\n");
 	ret = bootm_host_load_images(fit, cfg_noffset, is_spl);
 
 	return ret;
